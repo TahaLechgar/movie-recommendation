49b187baa33c643e10a7f5a2ed328f46c31d1583
TahaLechgar <tahamr08@gmail.com>
2023-03-26 13:40:39 +0000

    setup spring boot recommandation app

---
+++ recommandation/src/main/java/com/tahalechgar/recommandation/RecommandationApplication.java
@@ -0,0 +1,13 @@
+package com.tahalechgar.recommandation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecommandationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecommandationApplication.class, args);
+	}
+
+}
