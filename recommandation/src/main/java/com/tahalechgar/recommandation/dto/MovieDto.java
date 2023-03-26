c11e235f770a2191a40888a8b25d040984590995
TahaLechgar <tahamr08@gmail.com>
2023-03-26 14:45:59 +0000

    add movie dto to recommandation app

---
+++ recommandation/src/main/java/com/tahalechgar/recommandation/dto/MovieDto.java
@@ -0,0 +1,20 @@
+package com.tahalechgar.recommandation.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class MovieDto {
+    private Long id;
+
+    private String title;
+
+    private String genre;
+
+    private int releaseYear;
+
+    private String director;
+}
