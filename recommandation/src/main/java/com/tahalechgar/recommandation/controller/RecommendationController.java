16c1b660bb07e4252ebc54608be3a14fc0b67971
TahaLechgar <tahamr08@gmail.com>
2023-03-26 14:47:29 +0000

    add RecommandationController with /recommendations endpoint

--- recommandation/src/main/java/com/tahalechgar/recommandation/controller/RecommendationController.java
+++ recommandation/src/main/java/com/tahalechgar/recommandation/controller/RecommendationController.java
@@ -1,10 +1,30 @@
 package com.tahalechgar.recommandation.controller;
 
+import com.tahalechgar.recommandation.client.MovieClient;
+import com.tahalechgar.recommandation.dto.MovieDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-@RequestMapping("/recommandations")
-public class RecommandationController {
+import java.util.List;
 
+@RestController
+@RequestMapping("/recommendations")
+@RequiredArgsConstructor
+public class RecommendationController {
+
+    private final MovieClient movieClient;
+
+    @GetMapping
+    public ResponseEntity<List<MovieDto>> getRecommendations(@RequestParam(name = "genre") String genre) {
+        List<MovieDto> movies =  movieClient.findAllMovies();
+        List<MovieDto> recommendations = movies.stream()
+                .filter(movieDto -> genre.equals(movieDto.getGenre()))
+                .toList();
+
+        return ResponseEntity.ok().body(recommendations);
+    }
 }
